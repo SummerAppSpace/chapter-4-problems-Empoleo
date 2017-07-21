341a2b242ccfe3060116fbc73b9cf8a3c604583c
Empoleo <iggydowell@gmail.com>
2017-07-21 09:27:16 -0700

    Update Problem3_UsingHandwrittenModules.py

--- Problem3_UsingHandwrittenModules.py
+++ Problem3_UsingHandwrittenModules.py
@@ -4,7 +4,7 @@
 # A) Creating your module: Good news! We are already done with this, it is our code in problem 2!
 
 # B) Import the function plot_quasars from your module
-import Problem2_MoreFunctions.plot_quasars 
+import Problem2_MoreFunctions
 # C) Using your module: go ahead an use plot_quasars here.
 plot_quasars()
 # NOTE: if your module wasn't in the same directory as the directory from which you run the python interpreter, you will have to make sure the directory it is in is on the PYTHONPATH
